c1b4f83ec33b9e8f11be86340413465efd0c46f9
jeebaleepa <eehabarbash@gmail.com>
2021-05-02 00:21:49 +0300

    Login now stores the token

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -3,7 +3,7 @@ import CenteredContainer from "../components/CenteredContainer";
 import { Card, Button, Alert, Form } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import img from "../assets/img.jpg";
-import { AuthContext } from '../context/Context';
+import { AuthContext } from "../context/Context";
 
 export default function Login() {
   const emailRef = useRef();
@@ -16,51 +16,55 @@ export default function Login() {
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      setLoading(true)
-      setError('')
+      setLoading(true);
+      setError("");
       const endpoint = new URL("http://131.181.190.87:3000/user/login");
       const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: emailRef.current.value, password: passwordRef.current.value })
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+        }),
       };
-      let user = await fetch(endpoint, requestOptions);
-      if (user.status === 200) {
-        history.push('/')
-        setUser({ token: 1 })
+      let res = await fetch(endpoint, requestOptions);
+      if (res.status === 200) {
+        history.push("/");
+        let user = await res.json();
+        setUser({ token: user.token });
       }
     } catch (error) {
-      setError(error)
+      setError(error);
     }
   }
 
   return (
-    <div className='page-container'>
-        <Card>
-          <Card.Body className="card">
-            <h2 className="text-center mb-4">Login</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" required ref={emailRef} />
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label> password </Form.Label>
-                <Form.Control type="password" required ref={passwordRef} />
-              </Form.Group>
-              <Button disabled={loading} className="w-100" type="submit">
-                Login
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-        <div className="w-100 text-center mt-2 link1">
-          Need An Account ?{" "}
-          <Link to="/register">
-            <span className="reg">Register</span>
-          </Link>
-        </div>
+    <div className="page-container">
+      <Card>
+        <Card.Body className="card">
+          <h2 className="text-center mb-4">Login</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" required ref={emailRef} />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label> password </Form.Label>
+              <Form.Control type="password" required ref={passwordRef} />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2 link1">
+        Need An Account ?{" "}
+        <Link to="/register">
+          <span className="reg">Register</span>
+        </Link>
+      </div>
     </div>
   );
 }
